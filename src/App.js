d44dfd19f4c4d68fd53ee7f4631374edaef828e7
Viet My <Ziettmy@gmail.com>
2024-06-07 23:03:33 +0700

    add router about page and notfound page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar.js";
 import Search from "./components/users/Search.js";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import About from "./components/pages/About.js";
+import NotFound from "./components/pages/NotFound.js";
 const App = () => {
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -22,9 +24,10 @@ const App = () => {
       <Router>
         <Navbar />
         <div className='container'>
-          <h1>GitHub Users Data</h1>
           <Switch>
             <Route exact path='/' component={Search} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/*' component={NotFound} />
           </Switch>
         </div>
       </Router>
