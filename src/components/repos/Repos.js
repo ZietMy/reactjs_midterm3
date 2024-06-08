cd045f2ffe8fbeb9f154d9c61e6a2b41c8697875
Viet My <Ziettmy@gmail.com>
2024-06-09 00:01:33 +0700

    add file Repos and RepoItem

---
+++ src/components/repos/Repos.js
@@ -0,0 +1,21 @@
+import RepoItem from "./RepoItem";
+function Repos({ repos }) {
+  const listStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3,1fr)",
+    gridGap: "1rem",
+  };
+  return (
+    <>
+      <h2>User's Repos</h2>
+      <div style={listStyle}>
+        {repos.map((repo, index) => (
+          <RepoItem key={index} repo={repo} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+
+export default Repos;
