66b344d5bfe51b755447f598b8ef1df3932eff78
Viet My <Ziettmy@gmail.com>
2024-06-08 23:46:02 +0700

    refactor file app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// App.js
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/layout/Navbar.js";
-import Search from "./components/users/Search.js";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import About from "./components/pages/About.js";
-import NotFound from "./components/pages/NotFound.js";
-import User from "./components/users/User.js";
+import Home from "./components/layout/Home";
+import Navbar from "./components/layout/Navbar";
 const App = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
   return (
     <div className='App'>
       <Router>
         <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Search} />
-            <Route exact path='/about' component={About} />
-            <Route exact path="/user/:id" component={User} />
-            <Route exact path='/*' component={NotFound} />
-          </Switch>
-        </div>
+        <Home />
       </Router>
     </div>
   );
